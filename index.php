694a53231033a049c887673a0f699019991f86bb
derrodirik <f@roethke.info>
2017-04-15 22:31:39 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -22,10 +22,10 @@ $types = array('all','ban','temp_ban','mute','temp_mute','warning','temp_warning
 <html lang="en">
 	<head>
 		<title><?php echo $info['title']; ?></title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="/data/bootstrap.min.css">
+		<link rel="stylesheet" href="/data/font-awesome.min.css">
 		<script src="/data/jquery-3.1.1.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="/data/bootstrap.min.js"></script>
 		<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/<?php echo $info['theme']; ?>/bootstrap.min.css" rel="stylesheet">
 		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 	</head>
