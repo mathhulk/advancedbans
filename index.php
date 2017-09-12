1f446ddcbea14ded03f2588a68b48a4e3dc41c11
mathhulk <contact@theartex.net>
2017-09-11 20:54:17 -0700

    Added correct meta tags to HTML

--- index.php
+++ index.php
@@ -4,9 +4,39 @@ require("load.php");
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title><?php echo $info["messages"]["title"]; ?> - <?php echo $lang["punishments"]; ?></title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 		<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
 		
+		<!-- ICONS -->
+		<link rel="apple-touch-icon" sizes="57x57" href="assets/img/icons/apple-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="assets/img/icons/apple-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="assets/img/icons/apple-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="assets/img/icons/apple-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/img/icons/apple-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="assets/img/icons/apple-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="assets/img/icons/apple-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="assets/img/icons/apple-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="assets/img/icons/apple-icon-180x180.png">
+		<link rel="icon" type="image/png" sizes="192x192"  href="assets/img/icons/android-icon-192x192.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="assets/img/icons/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="96x96" href="assets/img/icons/favicon-96x96.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="assets/img/icons/favicon-16x16.png">
+		<meta name="msapplication-TileColor" content="#ffffff">
+		<meta name="msapplication-TileImage" content="assets/img/icons/ms-icon-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+		
+		<!-- INFORMATION -->
+		<meta name="description" content="<?php echo $info["messages"]["description"]; ?>">
+		<meta property="og:site_name" content="<?php echo $info["messages"]["title"]; ?>">
+		<meta property="og:title" content="<?php echo $lang["punishments"]; ?>">
+		<meta property="og:image" content="assets/img/icon.png">
+		<meta property="og:description" content="<?php echo $info["messages"]["description"]; ?>">
+		<meta property="og:url" content="http://<?php echo $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]; ?>">
+		<meta property="og:type" content="website">
+		
+		<title><?php echo $info["messages"]["title"]; ?> - <?php echo $lang["punishments"]; ?></title>
+		
+		<!-- GENERAL ICONS -->
 		<link rel="shortcut icon" href="assets/img/icon.png" type="image/x-icon">
 		<link rel="icon" href="assets/img/icon.png" type="image/x-icon">
 		
