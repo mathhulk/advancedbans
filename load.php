84f54cbc8114032a035112ab310220db81d13788
mathhulk <contact@theartex.net>
2017-10-15 09:13:42 -0700

    Specify default time zone for visitors

--- load.php
+++ load.php
@@ -11,14 +11,6 @@ error_reporting(0);
 session_start();
 
 /*
- *	FILES
- */
-require((in_array(array_pop(explode("/", $_SERVER['REQUEST_URI'])), array("user", "graphs")) ? "../inc/" : "inc/")."include/database.php");
-require((in_array(array_pop(explode("/", $_SERVER['REQUEST_URI'])), array("user", "graphs")) ? "../inc/" : "inc/")."include/info.php");
-require((in_array(array_pop(explode("/", $_SERVER['REQUEST_URI'])), array("user", "graphs")) ? "../inc/" : "inc/")."include/classes/pagination.class.php");
-require((in_array(array_pop(explode("/", $_SERVER['REQUEST_URI'])), array("user", "graphs")) ? "../inc/" : "inc/")."include/classes/date.class.php");
- 
-/*
  *	CONFIGURATION
  */
 $info = json_decode(file_get_contents((in_array(array_pop(explode("/", $_SERVER['REQUEST_URI'])), array("user", "graphs")) ? "../config.json" : "config.json"), FILE_USE_INCLUDE_PATH), true);
@@ -27,7 +19,15 @@ $info = json_decode(file_get_contents((in_array(array_pop(explode("/", $_SERVER[
  *	LANGUAGE
  */
 $lang = json_decode(file_get_contents((in_array(array_pop(explode("/", $_SERVER["REQUEST_URI"])), array("user", "graphs")) ? "../inc/languages/".(isset($_COOKIE["ab-lang"]) ? $_COOKIE["ab-lang"] : $info["default_language"]).".json" : "inc/languages/".(isset($_COOKIE["ab-lang"]) ? $_COOKIE["ab-lang"] : $info["default_language"]).".json"), FILE_USE_INCLUDE_PATH), true)["terms"];
-	
+
+/*
+ *	FILES
+ */
+require((in_array(array_pop(explode("/", $_SERVER['REQUEST_URI'])), array("user", "graphs")) ? "../inc/" : "inc/")."include/database.php");
+require((in_array(array_pop(explode("/", $_SERVER['REQUEST_URI'])), array("user", "graphs")) ? "../inc/" : "inc/")."include/info.php");
+require((in_array(array_pop(explode("/", $_SERVER['REQUEST_URI'])), array("user", "graphs")) ? "../inc/" : "inc/")."include/classes/pagination.class.php");
+require((in_array(array_pop(explode("/", $_SERVER['REQUEST_URI'])), array("user", "graphs")) ? "../inc/" : "inc/")."include/classes/date.class.php");
+ 
 /*
  *	PUNISHMENTS
  */
