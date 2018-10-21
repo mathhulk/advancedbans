0ce3d16d2354d9c1d04986ec3e7dca99e462d60d
mathhulk <no-reply@mathhulk.com>
2018-10-21 11:24:21 -0700

    Remove class reference for parse function

--- include/functions/parse.php
+++ include/functions/parse.php
@@ -1,7 +1,5 @@
 <?php
 
-use AdvancedBan;
-
 function parse(string $link) {
 	return ((AdvancedBan::getConfiguration( )))->get(["mod_rewrite"]) === true ? $link : "?request=" . str_replace("?", "&", $link);
 }
