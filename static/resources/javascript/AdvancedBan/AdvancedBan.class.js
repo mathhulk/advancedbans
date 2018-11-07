217db32e81d80dc1b29ceef51f6af867b0847477
mathhulk <no-reply@mathhulk.com>
2018-11-06 17:51:08 -0800

    Revert configuration reference to allow querying

--- static/resources/javascript/AdvancedBan/AdvancedBan.class.js
+++ static/resources/javascript/AdvancedBan/AdvancedBan.class.js
@@ -125,7 +125,7 @@ class AdvancedBan {
 	}
 	
 	static query( ) {
-		$.getJSON("https://use.gameapis.net/mc/query/players/" + this._configuration.get(["player_count", "server_ip"]), function(data) {
+		$.getJSON("https://use.gameapis.net/mc/query/players/" + AdvancedBan.configuration.get(["player_count", "server_ip"]), function(data) {
 			if(data.status === true) {
 				$(".players").text(data.players.online.toLocaleString( ));
 			} else {
